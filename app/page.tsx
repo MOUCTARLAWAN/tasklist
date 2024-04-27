0b271ffa702336f9e87e7c3f27480372f6e601b1
Mouctar <lawan.mouctar@ensi.ma>
2024-04-27 15:49:21 -0700

    Mise en place et affichage de la premiere page terminer

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
+'use client'
 import { useState, useEffect } from "react"
 
+import Header from "@/components/Header"
 export default function Home() {
 
   const [task, setTask] = useState("")
 
   return (
     <>
-      <Headers />
+      <Header />
     </>
   )
 }
