0402b1199c07b0e56d8fa798fd865802f0310610
Mouctar <lawan.mouctar@ensi.ma>
2024-04-29 00:18:21 -0700

    Mise en place du model Task terminer

---
+++ models/tasks.ts
@@ -0,0 +1,21 @@
+// Importer les bibliotheques necessaire depuis mongoose
+import { Schema, model, models } from 'mongoose'
+
+import { ITask } from '@/types'
+
+const taskSchema = new Schema<ITask>({
+    task: {
+        type: String,
+        required: [true, 'Task is required. '],
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
+
+
+// Creer un model mongoose base sur TaskSchema pour representer les taches. 
+const Task = models.task || model<ITask>('Task', taskSchema)
+
+export default Task;
